61d195d6658f98d3ccfb36053a36454ab8d14cb8
Georgy Khaydenko <ghaidenko@gmail.com>
2020-07-17 13:03:09 +0300

    add searchView layout

--- TestMusicPlayer/Search/SearchView.swift
+++ TestMusicPlayer/Search/SearchView.swift
@@ -10,10 +10,7 @@ import UIKit
 
 final class SearchView: UIView {
     var titleLabel: UILabel!
-    var sortedLabel: UILabel!
-    var switchControl: UISwitch!
-    var switchLeftLabel: UILabel!
-    var switchRightLabel: UILabel!
+    var searchTextField: UITextField!
     var tableView: UITableView!
     var activityIndicator: UIActivityIndicatorView!
 
@@ -28,33 +25,19 @@ final class SearchView: UIView {
     }
 
     private func layoutContent(in view: UIView) {
-        titleLabel = layout(UILabel(text: "Hotel List")) { make in
+        titleLabel = layout(UILabel(text: "Search for music")) { make in
             make.top.equalTo(safeArea).offset(20)
             make.centerX.equalToSuperview()
         }
 
-        sortedLabel = layout(UILabel(text: "sorted by:")) { make in
+        searchTextField = layout(UITextField()) { make in
             make.top.equalTo(titleLabel.bottom).offset(20)
-            make.centerX.equalToSuperview()
-        }
-
-        switchControl = layout(UISwitch()) { make in
-            make.top.equalTo(sortedLabel.bottom).offset(10)
-            make.centerX.equalToSuperview()
-        }
-
-        switchLeftLabel = layout(UILabel(text: "distance")) { make in
-            make.centerY.equalTo(switchControl)
-            make.trailing.equalTo(switchControl.leading).offset(-10)
-        }
-
-        switchRightLabel = layout(UILabel(text: "free rooms")) { make in
-            make.centerY.equalTo(switchControl)
-            make.leading.equalTo(switchControl.trailing).offset(10)
+            make.leading.equalToSuperview().offset(15)
+            make.trailing.equalToSuperview().offset(-15)
         }
 
         tableView = layout(UITableView()) { make in
-            make.top.equalTo(switchControl.bottom).offset(10)
+            make.top.equalTo(searchTextField.bottom).offset(10)
             make.leading.trailing.bottom.equalTo(safeArea)
         }
 
@@ -65,9 +48,12 @@ final class SearchView: UIView {
 
     private func applyStyle() {
         backgroundColor = .white
-        sortedLabel.font = .systemFont(ofSize: 13)
-        switchLeftLabel.font = .systemFont(ofSize: 13)
-        switchRightLabel.font = .systemFont(ofSize: 13)
+
+        searchTextField.autocorrectionType = .no
+        searchTextField.placeholder = "Enter music track name"
+
+        tableView.separatorStyle = .none
+
         if #available(iOS 13.0, *) {
             activityIndicator.style = UIActivityIndicatorView.Style.medium
         } else {
